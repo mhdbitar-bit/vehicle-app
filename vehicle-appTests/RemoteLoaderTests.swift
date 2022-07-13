bf8e228cf523bb05efd2153f9615c87ed82f0831
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-13 20:35:28 +0300

    Test request data from url

--- vehicle-appTests/RemoteLoaderTests.swift
+++ vehicle-appTests/RemoteLoaderTests.swift
@@ -1,6 +1,22 @@
 @testable import vehicle_app
 import XCTest
 
+final class RemoteLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func load(completion: @escaping (Result<[Point], Error>) -> Void) {
+        client.get(from: url) { result in
+            
+        }
+    }
+}
+
 final class RemoteLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
@@ -9,17 +25,27 @@ final class RemoteLoaderTests: XCTestCase {
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
+    func test_load_requestsDataFromURL() {
+        let url = anyURL()
+        let client = HTTPClientSpy()
+        let sut = RemoteLoader(url: url, client: client)
+        
+        sut.load { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
     // MARK: - Helpers
     
     class HTTPClientSpy: HTTPClient {
         private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
 
-        var requestedURLs: [URL] {  
+        var requestedURLs: [URL] {
             messages.map { $0.url }
         }
         
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-            
+            messages.append((url, completion))
         }
     }
 }
