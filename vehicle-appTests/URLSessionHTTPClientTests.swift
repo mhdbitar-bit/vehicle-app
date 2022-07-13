7af0730bf3098f94ad25e98805e54efac44b8b67
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-13 20:17:10 +0300

    Test success on HTTP URL response with data

--- vehicle-appTests/URLSessionHTTPClientTests.swift
+++ vehicle-appTests/URLSessionHTTPClientTests.swift
@@ -83,6 +83,17 @@ final class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(receivedValues?.response.statusCode, response.statusCode)
     }
     
+    func test_getFromURL_succeedsOnHTTPURLResponseWithData() {
+        let data = anyData()
+        let response = anyHTTPURLResponse()
+        
+        let receivedValues = resultValuesFor(data: data, response: response, error: nil)
+
+        XCTAssertEqual(receivedValues?.data, data)
+        XCTAssertEqual(receivedValues?.response.url, response.url)
+        XCTAssertEqual(receivedValues?.response.statusCode, response.statusCode)        
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
