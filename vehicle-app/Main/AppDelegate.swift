6f4d6d4f0a15d2dfc16ecd339b7b8e9c11eb2266
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-13 18:54:22 +0300

    Clean app the setup and remove unused code

---
+++ vehicle-app/Main/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  vehicle-app
+//
+//  Created by Mohammad Bitar on 7/13/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
---
+++ vehicle-app/Main/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  vehicle-app
+//
+//  Created by Mohammad Bitar on 7/13/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
