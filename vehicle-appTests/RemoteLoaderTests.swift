72195332cc01441683015abe40be88c00a9ab51c
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-14 08:14:24 +0300

    Delivers invalid data error on non 200 HTTP Response

--- vehicle-appTests/RemoteLoaderTests.swift
+++ vehicle-appTests/RemoteLoaderTests.swift
@@ -7,6 +7,7 @@ final class RemoteLoader {
     
     enum Error: Swift.Error {
         case connecitivy
+        case invalidData
     }
 
     
@@ -17,7 +18,12 @@ final class RemoteLoader {
     
     func load(completion: @escaping (Result<[Point], Error>) -> Void) {
         client.get(from: url) { result in
-            completion(.failure(.connecitivy))
+            switch result {
+            case .success:
+                completion(.failure(.invalidData))
+            case .failure:
+                completion(.failure(.connecitivy))
+            }
         }
     }
 }
@@ -72,12 +78,45 @@ final class RemoteLoaderTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_deliversInvalidDataErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        
+        let samples = [199, 201, 300, 400, 500]
+        
+        samples.enumerated().forEach { index, code in
+            expect(sut, toCompleteWith: .failure(.invalidData)) {
+                let json = makeItemsJSON([])
+                client.complete(withStatusCode: code, data: json, at: index)
+            }
+        }
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = anyURL(), file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteLoader(url: url, client: client)
         return (sut, client)
+    }
+    
+    private func expect(_ sut: RemoteLoader, toCompleteWith expectedResult: Result<[Point], RemoteLoader.Error>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            default:
+                XCTFail("Expected failure got \(receivedResult) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        action()
+        
+        waitForExpectations(timeout: 0.1)
     }
     
     class HTTPClientSpy: HTTPClient {
@@ -94,5 +133,19 @@ final class RemoteLoaderTests: XCTestCase {
         func complete(with error: Error, at index: Int = 0) {
             messages[index].completion(.failure(error))
         }
+        
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )!
+            messages[index].completion(.success((data, response)))
+        }
+    }
+    
+    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        return try! JSONSerialization.data(withJSONObject: items)
     }
 }
