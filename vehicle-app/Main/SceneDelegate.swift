ab79325a7a11fa7a404b6680458e724b8bdbf274
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-14 12:49:50 +0300

    Compose RemoteLoader with ListViewController

--- vehicle-app/Main/SceneDelegate.swift
+++ vehicle-app/Main/SceneDelegate.swift
@@ -11,9 +11,32 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    private let navigationController = UINavigationController()
+    let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        configureWindow()
+    }
+    
+    private func configureWindow() {
+        navigationController.setViewControllers([makeRootViewController()], animated: false)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+    private func makeRootViewController() -> ListViewController {
+        let coordinate1 = Coordinate(latitude: 53.694865, longitude: 9.757589)
+        let coordinate2 = Coordinate(latitude: 53.394655, longitude: 10.099891)
+        let remoteURL = URL(string: "https://api.mytaxi.com/poiservice/poi/v1")!
+        
+        let baseURL = VehicleEndpoint.getPoints.url(baseURL: remoteURL, coordinate1: coordinate1, coordinate2: coordinate2)
+        let loader = RemoteLoader(url: baseURL, client: remoteClient)
+        let viewModel = ListViewModel(loader: MainQueueDispatchDecorator(decoratee: loader))
+        let viewController = ListViewController(viewModel: viewModel, borderCoorindate: coordinate1)
+        return viewController
     }
 }
 
