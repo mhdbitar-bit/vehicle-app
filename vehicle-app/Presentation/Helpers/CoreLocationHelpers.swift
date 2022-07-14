05097afe502e800b28a9b832b438cd76ccc5f86e
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-14 11:17:23 +0300

    Add calculateDistanceBetween helper function

--- vehicle-app/Presentation/Helpers/CoreLocationHelpers.swift
+++ vehicle-app/Presentation/Helpers/CoreLocationHelpers.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import CoreLocation
+
+final class CoreLocationHelpers {
+    
+    static func calculateDistanceBetween(_ p1: Coordinate, _ p2: Coordinate) -> String {
+        let coordinate1 = CLLocation(latitude: p1.latitude, longitude: p1.longitude)
+        let coordinate2 = CLLocation(latitude: p2.latitude, longitude: p2.longitude)
+        
+        let distanceInMeters = coordinate1.distance(from: coordinate2)
+        let s = String(format: "%.2f", distanceInMeters)
+        
+        if distanceInMeters <= 1609 {
+            return s + " Meters"
+        } else {
+            return s + " Miles"
+        }
+    }
+}
