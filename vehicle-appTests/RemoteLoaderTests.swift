249d08528e93133c51838e13a84cc7b653a584f0
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-14 08:27:54 +0300

    Add PointMapper class and its decodable models

--- vehicle-appTests/RemoteLoaderTests.swift
+++ vehicle-appTests/RemoteLoaderTests.swift
@@ -1,6 +1,39 @@
 @testable import vehicle_app
 import XCTest
 
+final class PointMapper {
+    private struct list: Decodable {
+        private let poiList: [RemotePoint]
+    
+        private struct RemotePoint: Decodable {
+            let id: Int
+            let coordinate: RemoteCoordinate
+            let state: String
+            let type: String
+            let heading: Float
+        
+            struct RemoteCoordinate: Decodable {
+                let latitude: Float
+                let longitude: Float
+            }
+        }
+        
+        var points: [Point] {
+            return poiList.map { Point(
+                id: $0.id,
+                coordinate: Coordinate(
+                    latitude: $0.coordinate.latitude,
+                    longitude: $0.coordinate.longitude
+                ),
+                state: $0.state == "ACTIVE" ? .active : .inactive,
+                type: $0.type,
+                heading: $0.heading
+            )
+            }
+        }
+    }
+}
+
 final class RemoteLoader {
     private let url: URL
     private let client: HTTPClient
