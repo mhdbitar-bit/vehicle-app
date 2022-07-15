3fca47044cb3d95c17b0e7cb1aac3f8f2df87f62
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-15 16:39:15 +0300

    Test Composed of MapView

--- vehicle-appTests/MapViewControllerTests.swift
+++ vehicle-appTests/MapViewControllerTests.swift
@@ -1,9 +1,25 @@
 @testable import vehicle_app
 import XCTest
+import MapKit
 
 final class MapViewControllerTests: XCTestCase {
 
-    func test_zero() throws {
-        XCTFail("Tests not yet implemented in MapViewControllerTests")
+    func test_IsComposedOfMapView() {
+        let (sut, _) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertNotNil(sut.mapView)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: MapViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let viewModel = MapViewModel(loader: MainQueueDispatchDecorator(decoratee: loader), url: anyURL())
+        let sut = MapViewController(viewModel: viewModel)
+        trackForMemoryLeacks(loader, file: file, line: line)
+        trackForMemoryLeacks(sut, file: file, line: line)
+        return (sut, loader)
     }
 }
