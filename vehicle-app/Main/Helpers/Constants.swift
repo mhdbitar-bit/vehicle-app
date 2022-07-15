bf4e047220322c3a5da2d83cc74282d3318caa45
mhdbitar-bit <eng.mhd.bitar@gmail.com>
2022-07-15 16:01:47 +0300

    Add Constants helper

---
+++ vehicle-app/Main/Helpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  vehicle-app
+//
+//  Created by Mohammad Bitar on 7/15/22.
+//
+
+import Foundation
+
+let remoteURL = URL(string: "https://api.mytaxi.com/poiservice/poi/v1")!
+let southWeastCoordinate = Coordinate(latitude: 53.694865, longitude: 9.757589)
+let northEastCoordinate = Coordinate(latitude: 53.394655, longitude: 10.099891)
